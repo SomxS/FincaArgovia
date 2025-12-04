3c91ad70e1d6a9ddeeaa0d829287c2c6b02459e9
rosaangelicadev <rosaangelicadev@gmail.com>
2025-12-03 21:27:25 -0600

    Fix Navbar in Almacén

--- ERP3/operacion/almacen/catalogo.php
+++ ERP3/operacion/almacen/catalogo.php
@@ -12,7 +12,6 @@ require_once('layout/core-libraries.php');
 ?>
 
 <!-- CoffeeSoft Framework -->
-   <!-- Compact Styles -->
 <script src="../../src/js/coffeeSoft.js"></script>
 <script src="https://rawcdn.githack.com/SomxS/Grupo-Varoch/refs/heads/main/src/js/plugins.js"></script>
 <script src="https://www.plugins.erp-varoch.com/ERP/JS/complementos.js"></script>
@@ -22,6 +21,14 @@ require_once('layout/core-libraries.php');
     <div id="menu-sidebar"></div>
     <main>
         <div id="main__content">
+            <!-- Breadcrumb Navigation -->
+            <nav aria-label="breadcrumb">
+                <ol class="breadcrumb">
+                    <li class="breadcrumb-item text-uppercase text-muted">Operación</li>
+                    <li class="breadcrumb-item fw-bold active">Catalogo</li>
+                </ol>
+            </nav>
+            <!-- Contenedor principal -->
             <div class="" id="root"></div>
         </div>
     </main>
